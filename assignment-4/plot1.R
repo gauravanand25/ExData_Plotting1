d712afa8d1ca93688da7b3eb22d3ad69ad745c5d
gauravanand25 <gauravanand25@gmail.com>
2016-02-16 22:12:55 +0530

    first graph, emissions PM2.5 has decreased

---
+++ assignment-4/plot1.R
@@ -0,0 +1,8 @@
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds") # emissions data
+SCC <- readRDS("Source_Classification_Code.rds")
+
+meanYearWise <- lapply( split( NEI$Emissions, NEI$year), mean)
+barplot(as.numeric(meanYearWise), names.arg=c(levels(factor(NEI$year))), xlab="Years", ylab="Emissions Levels")
+dev.copy(png, file = "plot1.png")
+dev.off()
