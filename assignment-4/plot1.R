b0f1294c4fb2d1b3c6c3d19350be26b43a2da416
gauravanand25 <gauravanand25@gmail.com>
2016-02-16 22:41:05 +0530

    backgroud white added

--- assignment-4/plot1.R
+++ assignment-4/plot1.R
@@ -2,6 +2,7 @@
 NEI <- readRDS("summarySCC_PM25.rds") # emissions data
 SCC <- readRDS("Source_Classification_Code.rds")
 
+par(bg="white")
 meanYearWise <- lapply( split( NEI$Emissions, NEI$year), mean)
 barplot(as.numeric(meanYearWise), names.arg=c(levels(factor(NEI$year))), xlab="Years", ylab="Emissions Levels")
 dev.copy(png, file = "plot1.png")
